4e7b1cca1b9ddc828d77049545ab33154b0387d7
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-02-23 09:21:45 +0000

    final update 2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-// Structure to pass data to threads
 struct ThreadData {
     vector<string> words;
     map<string, int> wordCount;
@@ -14,51 +13,44 @@ struct ThreadData {
 
 // Thread function
 void* countWords(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
 
-    ThreadData* data = (ThreadData*) arg;
-
-    for (int i = 0; i < data->words.size(); i++) {
-        data->wordCount[data->words[i]]++;
+    for (string word : data->words) {
+        data->wordCount[word]++;
     }
 
     pthread_exit(NULL);
 }
 
 int main() {
+    int N;
+    cout << "Enter number of threads: ";
+    cin >> N;
 
     ifstream file("input.txt");
-
     if (!file) {
-        cout << "Cannot open file\n";
+        cout << "Error opening file!\n";
         return 1;
     }
 
+    // Read all words from file
     vector<string> allWords;
     string word;
-
-    // Read file words
     while (file >> word) {
         allWords.push_back(word);
     }
-
     file.close();
 
-    int N = 2;
-    int segmentSize = allWords.size() / N;
+    int totalWords = allWords.size();
+    int segmentSize = totalWords / N;
 
-    pthread_t threads[N];
-    ThreadData threadData[N];
+    vector<pthread_t> threads(N);
+    vector<ThreadData> threadData(N);
 
     // Divide words among threads
     for (int i = 0; i < N; i++) {
-
         int start = i * segmentSize;
-        int end;
-
-        if (i == N - 1)
-            end = allWords.size();
-        else
-            end = start + segmentSize;
+        int end = (i == N - 1) ? totalWords : start + segmentSize;
 
         for (int j = start; j < end; j++) {
             threadData[i].words.push_back(allWords[j]);
@@ -67,23 +59,30 @@ int main() {
         pthread_create(&threads[i], NULL, countWords, &threadData[i]);
     }
 
-    // Wait for threads to finish
+    // Wait for all threads
     for (int i = 0; i < N; i++) {
         pthread_join(threads[i], NULL);
     }
 
+    //Print Intermediate Results
+    cout << "\n--- Intermediate Results ---\n";
+    for (int i = 0; i < N; i++) {
+        cout << "\nThread " << i + 1 << ":\n";
+        for (auto pair : threadData[i].wordCount) {
+            cout << pair.first << " : " << pair.second << endl;
+        }
+    }
+
     // Merge results
     map<string, int> finalCount;
-
     for (int i = 0; i < N; i++) {
         for (auto pair : threadData[i].wordCount) {
             finalCount[pair.first] += pair.second;
         }
     }
 
-    // Print final results
-    cout << "\nFinal Word Frequency:\n";
-
+    // Print final result
+    cout << "\n--- Final Word Frequency ---\n";
     for (auto pair : finalCount) {
         cout << pair.first << " : " << pair.second << endl;
     }
